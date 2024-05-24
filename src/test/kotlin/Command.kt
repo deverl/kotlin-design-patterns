03efd95fd7f589e2485b1a25c718c59bb603e889
DeVerl Stokes <deverl_stokes@intuit.com>
2024-05-24 13:24:40 -0600

    Working on command, adding strategy.

--- src/test/kotlin/Command.kt
+++ src/test/kotlin/Command.kt
@@ -1,1 +1,49 @@
+import kotlin.test.Test
+
 // Command design pattern
+// Encapsulate a request as an object that contains all the information about the request
+// Decouple sender from receiver
+// The command object is then passed to the correct handler
+
+interface Command {
+    fun execute()
+}
+
+
+class OrderAddCommand(val id: Long) : Command {
+    override fun execute() {
+        println("Adding order with id $id")
+    }
+}
+
+
+class OrderPayCommand(val id: Long) : Command {
+    override fun execute() {
+        println("Paying for order with id $id")
+    }
+}
+
+
+class CommandProcessor {
+    private val q = mutableListOf<Command>()
+    fun addToQueue(command: Command): CommandProcessor = apply { q.add(command) }
+    fun processCommands(): CommandProcessor = apply {
+        q.forEach { it.execute() }
+        q.clear()
+    }
+}
+
+
+// Test
+
+class CommandTest {
+    @Test
+    fun testCommand() {
+        CommandProcessor()
+            .addToQueue(OrderAddCommand(1))
+            .addToQueue(OrderAddCommand(2))
+            .addToQueue(OrderPayCommand(2))
+            .addToQueue(OrderPayCommand(1))
+            .processCommands()
+    }
+}
