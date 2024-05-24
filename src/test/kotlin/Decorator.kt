c6ba746df134ed7612f93fd45220862a6490f8dd
DeVerl Stokes <deverl_stokes@intuit.com>
2024-05-24 10:24:31 -0600

    Adding more patterns.

---
+++ src/test/kotlin/Decorator.kt
@@ -0,0 +1,67 @@
+import kotlin.test.Test
+
+// Attach new behavior to an object
+// Without altering existing code
+// Over riding existing behavior
+
+
+interface CoffeeMaker {
+    fun makeSmallCoffee()
+    fun makeLargeCoffee()
+}
+
+
+class NormalCoffeeMaker : CoffeeMaker {
+    override fun makeSmallCoffee() {
+        println("Normal: Making small coffee")
+    }
+
+    override fun makeLargeCoffee() {
+        println("Normal: Making large coffee")
+    }
+}
+
+
+
+// Decorator
+
+class EnhancedCoffeeMaker(private val coffeeMaker: CoffeeMaker) : CoffeeMaker by coffeeMaker {
+    fun makeMilkCoffee() {
+        println("Enhanced: Making milk coffee")
+        coffeeMaker.makeLargeCoffee()
+        println("Enhanced: Adding milk")
+    }
+
+    fun makeSugarCoffee() {
+        println("Enhanced: Making sugar coffee")
+        coffeeMaker.makeSmallCoffee()
+        println("Enhanced: Adding sugar")
+    }
+
+    override fun makeLargeCoffee() {
+        println("Enhanced: Making large coffee")
+    }
+
+//    override fun makeSmallCoffee() {
+//        println("Enhanced: Making small coffee")
+//    }
+}
+
+
+class DecoratorTest {
+    @Test
+    fun testDecorator() {
+        val normalCoffeeMaker = NormalCoffeeMaker()
+        val enhancedCoffeeMaker = EnhancedCoffeeMaker(normalCoffeeMaker)
+
+        println("----------------------------")
+        enhancedCoffeeMaker.makeSmallCoffee()
+        println("----------------------------")
+        enhancedCoffeeMaker.makeLargeCoffee()
+        println("----------------------------")
+        enhancedCoffeeMaker.makeMilkCoffee()
+        println("----------------------------")
+        enhancedCoffeeMaker.makeSugarCoffee()
+        println("----------------------------")
+    }
+}
