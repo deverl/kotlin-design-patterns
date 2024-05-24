a83a0768fc246685f738f4c7c759ee37606a1fae
DeVerl Stokes <deverl_stokes@intuit.com>
2024-05-24 14:36:05 -0600

    Adding Mediator design pattern

---
+++ src/test/kotlin/Mediator.kt
@@ -0,0 +1,4 @@
+// Mediator design pattern
+// Provides a centralized communication medium between different objects in a system
+// Objects no longer communicate directly with each other, but instead communicate through the mediator
+// Reduces the dependencies between communicating objects
