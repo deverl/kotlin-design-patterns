cca2aef02569f19fa26765856725cfb4ec8b9dd8
DeVerl Stokes <deverl_stokes@intuit.com>
2024-05-24 12:19:26 -0600

    Adding comments

--- src/test/kotlin/ChainOfResponsibility.kt
+++ src/test/kotlin/ChainOfResponsibility.kt
@@ -1,2 +1,5 @@
 // Chain of Responsibility Pattern
 // Define a chain of receiver handlers to process a request
+// Each handler contains a reference to the next handler in the chain
+// Each handler decides either to process the request or to pass it to the next handler in the chain
+// Requests can be of any type
